2e7ec031081a3c1da7c87555625b0d2280b22166
vmihda <vitalik.migda@gmail.com>
2019-05-25 11:52:26 +0300

    add registration users

--- app/blogengine/blogengine/urls.py
+++ app/blogengine/blogengine/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import redirect_block
+from users import views as usersView
 
 urlpatterns = [
     path('', redirect_block),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('registration/', usersView.signup, name="registration"),
 ]
